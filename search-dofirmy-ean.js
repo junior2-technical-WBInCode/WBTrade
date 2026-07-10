39b07b0e91b095a317352ee478eedc1e3cacea74
Jakub Maziarz <technical-support-junior@wb-partners.pl>
2026-07-10 14:57:15 +0200

    chore: usunięcie starych, nieużywanych skryptów debugowych z katalogu głównego

--- search-dofirmy-ean.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-function main() {
-  const xmlContent = fs.readFileSync('sample_do_firmy.xml', 'utf-8');
-  console.log("Does DoFirmy XML contain '5906142964109'?", xmlContent.includes('5906142964109'));
-  console.log("Does DoFirmy XML contain 'G157-100'?", xmlContent.includes('G157-100'));
-}
-
-main();
